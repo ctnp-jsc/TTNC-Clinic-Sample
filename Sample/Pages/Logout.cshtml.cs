f68e2ceb1c7a279e2f5342239b2b157ff306fd3c
minhvtb-ctnp <minhvtb@ctnpsolutions.com>
2023-10-24 14:08:37 +0700

    update proxy

--- Sample/Pages/Logout.cshtml.cs
+++ Sample/Pages/Logout.cshtml.cs
@@ -3,18 +3,27 @@ using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authentication.Cookies;
 using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc.RazorPages;
+using Sample.Extensions;
 
 namespace Sample.Pages;
 
 public class LogoutModel : PageModel
 {
+    private readonly IConfiguration configuration;
+
+    public LogoutModel(IConfiguration configuration) =>
+        this.configuration = configuration;
+
     [Authorize]
     public async Task OnGetAsync()
     {
         var authenticationProperties = new LogoutAuthenticationPropertiesBuilder()
            .WithRedirectUri("/")
            .Build();
-
+        if (configuration.IsUnderProxy())
+        {
+            HttpContext.Request.IsHttps = true;
+        }
         await HttpContext.SignOutAsync(Auth0Constants.AuthenticationScheme, authenticationProperties);
         await HttpContext.SignOutAsync(CookieAuthenticationDefaults.AuthenticationScheme);
     }
